48a3d70b5193fd19c6e47064c0bdb9207be9a27b
winjun1128 <hsj1128ty@naver.com>
2025-04-28 16:56:24 +0900

    클래스 시작, 필드 && 생성자

--- src/study/refer/Reference07.java
+++ src/study/refer/Reference07.java
@@ -27,7 +27,16 @@ public class Reference07 {
 		if(day4==Week.FRI) {
 			System.out.println("금요일이구나~");
 		}
+//		if(Week.FRI.equals("FRI")) {							//열거형은 문자열이 아님
+//			System.out.println("금요일이구나~");
+//		}
 		Topping tp1 = Topping.SUCREAM;
+		
+		//String str = Week.FRI;									//열거형을 문자열로 넣기 바로 불가
+		String str = Week.FRI.toString();							//toString()으로
+		System.out.println(str);
+		
+		//열거형의 용도는?->값이 정해져있을때 입력 실수하지 않기 위해
 	}
 
 }
