4abec4ba8d20dbd95fb86849761d4191e47d1ed2
winjun1128 <hsj1128ty@naver.com>
2025-05-12 15:45:53 +0900

    연습43 푸드트럭 리팩토링

---
+++ src/study/practice/Practice43ANS/SalesInfo.java
@@ -0,0 +1,9 @@
+package study.practice.Practice43ANS;
+
+public class SalesInfo {
+	//일자별 매출정보
+	String salesDate; //"20250512" 20221211
+	int totalSales; //매출
+	int lossSales; //손해금액
+	int income;	//최종 수익
+}
