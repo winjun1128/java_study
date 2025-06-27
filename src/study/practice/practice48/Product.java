f2ee83667e793cf9e7e389f57e6e0c9cd7f18613
winjun1128 <hsj1128ty@naver.com>
2025-06-27 16:19:44 +0900

    연습48

---
+++ src/study/practice/practice48/Product.java
@@ -0,0 +1,42 @@
+package study.practice.practice48;
+
+
+//DB 테이블에서 읽어온 데이터를 저장하는 역할
+//DTO (Data Transfer Object)
+public class Product {
+	int p_code;
+	String p_name;
+	int p_price;
+	public Product() {}
+	
+	public Product(int p_code,String p_name,int p_price) {
+		this.p_code = p_code;
+		this.p_name = p_name;
+		this.p_price = p_price;
+	}
+
+	public int getP_code() {
+		return p_code;
+	}
+
+	public void setP_code(int p_code) {
+		this.p_code = p_code;
+	}
+
+	public String getP_name() {
+		return p_name;
+	}
+
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+
+	public int getP_price() {
+		return p_price;
+	}
+
+	public void setP_price(int p_price) {
+		this.p_price = p_price;
+	}
+
+}
