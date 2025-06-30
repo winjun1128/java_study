ad10a0cff77e70931b3b6b21106d16a74cfc023c
winjun1128 <hsj1128ty@naver.com>
2025-06-30 10:13:34 +0900

    연습49 DB 테이블 조회 연동

---
+++ src/study/practice/practice49/Practice49.java
@@ -0,0 +1,80 @@
+package study.practice.practice49;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import study.db.v2.DBConnectionManager;
+import study.db.v2.Dept;
+import study.practice.practice48.Product;
+
+public class Practice49 {
+
+	public static void main(String[] args) {
+		List<Professor> pList = findProfessorListByDeptno(201);
+		for(Professor p:pList) {
+			System.out.println(p.toString());
+		}
+
+	}
+	public static List<Professor> findProfessorListByDeptno(int deptno){
+		// DB연결 및 사용시 필요한 객체
+		Connection conn = null; // DB 연결 객체
+		PreparedStatement psmt = null; // DB 연결후, sql 명령 실행해주는 객체
+		ResultSet rs = null; // sql Select 실행 후 조회 결과가 저장되는 객체
+		
+		conn = DBConnectionManager.connectDB();
+		
+		// 쿼리 준비
+		//String query = "select * from professor deptno = ?";
+		
+		String query = "select profno, name, id, position, pay,deptno "
+				+ " from professor where deptno = ?";
+		
+		//List<Dept> deptList = null;
+		List<Professor> professorList = new ArrayList<Professor>();
+		
+		try {
+			
+			psmt = conn.prepareStatement(query); // 쿼리실행 준비객체
+			
+			psmt.setInt(1, deptno);
+			rs = psmt.executeQuery(); // 쿼리 실행 후 결과 저장
+			
+			// ResultSet rs 에 담겨져있는 쿼리 수행결과 확인
+			while (rs.next()) { // 읽어온 데이터를 행 단위로 반복하면서 접근
+				// rs.next() 다음 불러올 행 데이터가 있으면 true, 없으면 false
+		
+				// 컬럼인덱스 / 컬럼명
+				// rs.getInt(1)
+				
+		//		if(deptList == null)
+		//			deptList = new ArrayList<Dept>();
+				
+				// 해당 행에 컬럼 단위로 데이터 접근
+				Professor p = new Professor(
+						rs.getInt("profno"), 
+						rs.getString("name"), 
+						rs.getString("id"),
+						rs.getString("position"),
+						rs.getInt("pay"),
+						rs.getInt("deptno"));
+				professorList.add(p);
+			}
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		DBConnectionManager.disconnectDB(conn, psmt, rs);
+		
+		return professorList;
+	}
+
+
+}
